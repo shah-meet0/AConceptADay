11de79d6a9001ab2215f1dc4be81747a91265069
Meet Shah <meetsamshah@gmail.com>
2023-09-26 12:02:10 +0100

    added basic parsing functionality

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,1 +1,36 @@
 import streamlit as st
+import os
+from utilities import parse_post
+
+
+@st.cache_data
+def parse_posts():
+    categories = {'All': {}}  # Dictionary: String, Dictionary
+    posts = os.listdir('./posts')
+    for post in posts:
+        with open(f'./posts/{post}', 'rb') as f:
+            temp_post = parse_post(f)
+            if temp_post.category.title() in categories.keys():
+                post_dict = categories.get(temp_post.category.title())
+                post_dict[temp_post.title.title()] = temp_post
+                categories['All'][temp_post.title.title()] = temp_post
+            else:
+                categories[temp_post.category.title()] = {temp_post.title.title(): temp_post}
+                categories['All'][temp_post.title.title()] = temp_post
+    return categories
+
+
+# ******************************************START OF APP******************************************************
+
+# Title Space
+
+st.title('A Concept a Day')
+st.subheader('By Meet Shah')
+
+# Post Import Space
+category_selection = parse_posts()
+
+
+# Sidebar
+category = st.sidebar.selectbox(label='Topic', options=category_selection.keys())
+post_selection = st.sidebar.selectbox(label='Posts', options=category_selection[category])
--- utilities.py
+++ utilities.py
@@ -1,19 +1,28 @@
 from datetime import datetime
-
+from datetime import timezone
+import pickle
 
 class Post:
 
     def __init__(self, author, title, content, category):
-        self.time_created = datetime.now()
+        self.time_created = datetime.now(tz=timezone.utc)
         self.author = author
         self.title = title
         self.content = content # In Markdown format
         self.category = category
 
     def save(self):
-        pass  # to ./posts in a way that streamlit can import it
+        with open(f'./posts/{self.title.lower()}.pkl', 'wb') as p:
+            pickle.dump(self, p)
 
     def import_from_word(self, filepath):
         pass
 
+    def __str__(self):
+        return (f'#H1 {self.title} \n' +
+                f'#H2 {self.time_created.strftime("%a %d %b %Y %I:%M %p")} \n' +
+                self.content)
 
+
+def parse_post(post_file):
+    return pickle.load(post_file)
