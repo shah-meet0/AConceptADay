4b015dc3cff17e19c6b9b4543b06808bd0179435
Meet Shah <meetsamshah@gmail.com>
2023-09-26 17:37:58 +0100

    added basic parsing functionality

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,36 +1,44 @@
 import streamlit as st
 import os
 from utilities import parse_post
+st.set_page_config(page_title="A Concept a Day")
 
 
 @st.cache_data
-def parse_posts():
+def parse_all_posts():
     categories = {'All': {}}  # Dictionary: String, Dictionary
     posts = os.listdir('./posts')
     for post in posts:
-        with open(f'./posts/{post}', 'rb') as f:
-            temp_post = parse_post(f)
-            if temp_post.category.title() in categories.keys():
-                post_dict = categories.get(temp_post.category.title())
-                post_dict[temp_post.title.title()] = temp_post
-                categories['All'][temp_post.title.title()] = temp_post
-            else:
-                categories[temp_post.category.title()] = {temp_post.title.title(): temp_post}
-                categories['All'][temp_post.title.title()] = temp_post
+        temp_post = parse_post('./posts/' + post)
+        if temp_post.category.title() in categories.keys():
+            post_dict = categories.get(temp_post.category.title())
+            post_dict[temp_post.title.title()] = temp_post
+            categories['All'][temp_post.title.title()] = temp_post
+        else:
+            categories[temp_post.category.title()] = {temp_post.title.title(): temp_post}
+            categories['All'][temp_post.title.title()] = temp_post
     return categories
-
 
 # ******************************************START OF APP******************************************************
 
-# Title Space
-
-st.title('A Concept a Day')
-st.subheader('By Meet Shah')
 
 # Post Import Space
-category_selection = parse_posts()
+category_selection = parse_all_posts()
 
 
 # Sidebar
-category = st.sidebar.selectbox(label='Topic', options=category_selection.keys())
-post_selection = st.sidebar.selectbox(label='Posts', options=category_selection[category])
+category_selected = st.sidebar.selectbox(label='Topic', options=category_selection.keys())
+post_selected = st.sidebar.selectbox(label='Posts', options=category_selection[category_selected])
+
+show_title = st.sidebar.checkbox('Show Title', value=True)
+
+# Main Page
+
+# Title Space
+if show_title:
+    st.title('A Concept a Day')
+    st.write('By Meet Shah')
+
+
+st.write(category_selection[category_selected][post_selected].__str__())
+
--- utilities.py
+++ utilities.py
@@ -1,11 +1,14 @@
 from datetime import datetime
-from datetime import timezone
 import pickle
-
+import mammoth
+import pandoc
+import os
+from zoneinfo import ZoneInfo
+import shutil
 class Post:
 
     def __init__(self, author, title, content, category):
-        self.time_created = datetime.now(tz=timezone.utc)
+        self.time_created = datetime.now(ZoneInfo('Europe/London'))
         self.author = author
         self.title = title
         self.content = content # In Markdown format
@@ -15,14 +18,61 @@ class Post:
         with open(f'./posts/{self.title.lower()}.pkl', 'wb') as p:
             pickle.dump(self, p)
 
-    def import_from_word(self, filepath):
-        pass
+    def export_to_word(self):
+        filepath = f'./word_files/{self.title.lower()}.docx'
+        with open(filepath, 'wb') as wf:
+            pandoc.write(pandoc.read(self.content), file=wf, format='docx')
+        return filepath
+
+    def export_to_word_and_save(self):
+        filepath = self.export_to_word()
+        os.startfile(os.path.normpath(filepath)) # For some reason os isn't just able to read the path
+        input('Press Enter when done editing')
+        import_from_word(filepath, self.title, self.category, self.author)
+        print('Edited and Saved')
 
     def __str__(self):
-        return (f'#H1 {self.title} \n' +
-                f'#H2 {self.time_created.strftime("%a %d %b %Y %I:%M %p")} \n' +
+        return (f'### {self.title} \n' +
+                f'###### {self.time_created.strftime("%a %d %b %Y %I:%M %p")} \n' +
                 self.content)
 
 
-def parse_post(post_file):
-    return pickle.load(post_file)
+def parse_post(filepath):
+    with open(filepath, 'rb') as f:
+        parsed_post = pickle.load(f)
+    return parsed_post
+
+
+def import_from_word(filepath, title, category, author='Meet Shah'):
+    with open(filepath, 'rb') as docx_file:
+        content_in_markdown = mammoth.convert_to_markdown(
+            docx_file, convert_image=mammoth.images.img_element(ImageWriter(title, './assets/images/'))).value
+    converted_contents = pandoc.write(pandoc.read(content_in_markdown), format='markdown')
+
+    imported_post = Post(author, title, converted_contents, category)
+    imported_post.save()
+    return imported_post
+
+
+# Copied mostly from mammoth documentation with some additions
+class ImageWriter(object):
+    def __init__(self, prefix, output_dir):
+        self._prefix = prefix
+        self._output_dir = output_dir
+        self._image_number = 1
+
+    def __call__(self, element):
+        extension = element.content_type.partition("/")[2]
+        image_filename = "{0}_{1}.{2}".format(self._prefix, self._image_number, extension)
+        with open(os.path.join(self._output_dir, image_filename), "wb") as image_dest:
+            with element.open() as image_source:
+                shutil.copyfileobj(image_source, image_dest)
+
+        self._image_number += 1
+
+        return {"src": self._output_dir + image_filename}
+
+
+
+
+
